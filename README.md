e97fe7a7dc409eb363fc94464b0916a279e6ff76
ademar2008 <ademar.senger@escola.pr.gov.br>
2024-09-11 21:09:35 -0300

    index

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # js-escrevenom
+html, body {
+  margin: 0;
+  padding: 0;
+}
+canvas {
+  display: block;
+}
---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(6000, 6000);
+    backgr};ound("#673Ae7E"); 
+  
+  
+  function draw() {
+    stroke("#06ECA50");
+    fill("#8B34Ag  22");
+      
+  
+    // console.log(mouseIsPressed)
+  
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
+  
